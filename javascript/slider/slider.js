8787dcef654a0bbdf1ede1454e231d24523ec7e0
Allan <mingxiaozhou88@gmail.com>
2011-08-18 23:18:43 +0800

    Achieve scrolling

--- javascript/slider/slider.js
+++ javascript/slider/slider.js
@@ -1,20 +1,50 @@
-$(function(){
-	var sw=0;
-	$(".demo .num a").mouseover(function(){
-		sw=$(".num a").index(this);
-		myShow(sw);
-	});
-	function myShow(i){
-		$(".demo .num a").eq(i).addClass("cur").siblings("a").removeClass("cur");
-		$(".demo ul li").eq(i).stop(true,true).fadeIn(600).siblings("li").fadeOut(600);
-	}
-	$(".demo").hover(
-		function(){
-			if(myTime){clearInterval(myTime)}
-		},
-		function(){
-			myTime=setInterval(function(){myShow(sw);sw++;if(sw==5){sw=0}},2000);
+function slider(config){
+	this.init(config);
+}
+slider.prototype = {
+	timer : null,
+	curItem : 0,
+	selected : "",
+	picObj : "",
+	numObj : "",
+	total : 0,
+	init : function(config){
+		this.selected = config.selected;
+		this.picObj = config.picObj;
+		this.numObj = config.numObj;
+		this.total = $(this.picObj).size();
+		this.initElements();
+		this.run();
+	},
+	initElements : function(){
+		var _this = this,numStr = "",zNum = this.total;
+		for(var i=0; i<this.total; i++){
+			if(i == 0){
+				numStr += '<a class="'+ _this.selected  +'">'+ (i+1)  +'</a>';
+			}else{
+				numStr += '<a>'+ (i+1)  +'</a>';
+			}
 		}
-	);
-});
-
+		$(this.picObj).each(function(i){$(this).css("z-index",zNum--);});
+		$(this.numObj).html(numStr);
+		$(this.numObj + " a").bind("mouseenter",this,this.onMeHandler);
+	},
+	onMeHandler : function(event){
+		var _this = event.data;
+		_this.curItem = $(this).index();
+		_this.show(_this.curItem);
+	},
+	show : function(num){
+		$(this.numObj + " a").removeClass().eq(num).addClass(this.selected);
+		$(this.picObj).eq(num).stop(true,true).fadeIn(600).siblings().fadeOut(600);
+	},
+	run : function(){
+		var _this = this;
+		this.timer = setInterval(function(){
+			_this.show(_this.curItem);
+			_this.curItem++;
+			if(_this.curItem==_this.total){_this.curItem=0}
+		},2000);
+	}
+}
+var s = new slider({picObj : ".demo ul li",numObj : ".num",selected : "cur"});
